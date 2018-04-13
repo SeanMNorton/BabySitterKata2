99fdae5ca315f80c348513bc77fe5ec6ab977687
Sean Norton <Norton.Sean.M@gmail.com>
2018-04-13 15:36:42 -0500

    change wording on initalizer tests

--- spec/sitter_calculator_spec.rb
+++ spec/sitter_calculator_spec.rb
@@ -3,15 +3,15 @@ require 'sitterCalculator.rb'
 describe SitterCalculator do
   context "when creating a new SitterCalculator" do
     before { @calc = SitterCalculator.new([5, 7, 10]) }
-    it "is initalized with startTime" do
+    it "is initalized with a startTime" do
       expect(@calc.startTime).to eq(5)
     end
 
-    it "is initalized with bedTime" do
+    it "is initalized with a bedTime" do
       expect(@calc.bedTime).to eq(7)
     end
 
-    it "is initalized with endTime" do
+    it "is initalized with a endTime" do
       expect(@calc.endTime).to eq(10)
     end
   end
