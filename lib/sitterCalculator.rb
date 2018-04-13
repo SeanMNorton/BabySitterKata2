24e1f7b734bfc4826f145de6603403d64a4774b5
Sean Norton <Norton.Sean.M@gmail.com>
2018-04-13 15:29:52 -0500

    add startTime to SitterCalc

--- lib/sitterCalculator.rb
+++ lib/sitterCalculator.rb
@@ -1,3 +1,6 @@
 class SitterCalculator
-  
+  attr_reader :startTime
+  def initialize(arguments)
+    @startTime = arguments[0]
+  end
 end
--- spec/sitter_calculator_spec.rb
+++ spec/sitter_calculator_spec.rb
@@ -1,5 +1,10 @@
 require 'sitterCalculator.rb'
 
 describe SitterCalculator do
-
+  context "when creating a new SitterCalculator" do
+    before { @calc = SitterCalculator.new([5, 7, 10]) }
+    it "is initalized with startTime" do
+      expect(@calc.startTime).to eq(5)
+    end
+  end
 end
